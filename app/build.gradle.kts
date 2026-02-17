dd6d2dfd69a753bcf47a64f513129de3b56dda95
Hui-Hong You <hiroshi@ghostsinthelab.org>
2026-02-17 22:40:52 +0800

    Release 0.0.3
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.2"
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
